b543f49dd9b17a183db0ed78acc8e68ba7cff5dd
Luide Lima <luizcarloslimadasilva1@gmail.com>
2023-09-05 16:13:01 -0300

    first commit

---
+++ src/PlanoOperadora.java
@@ -0,0 +1,20 @@
+//Cenário proposto para Switch case
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "T"; // M / T
+
+        switch (plano) {
+            case "T":{
+                System.out.println("5Gb de YouTube");
+            }
+            case "M":{
+               System.out.println("WhatsApp e Instagram grátis");
+            }    
+            case "B":{
+               System.out.println("100 minutos de ligação");
+            
+            }
+        }
+       
+    }  
+}  
---
+++ src/ResultadoEscolar3.java
@@ -0,0 +1,12 @@
+//controle condicional temária
+public class ResultadoEscolar3 {
+    public static void main(String[] args) {
+        int nota = 9;
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+
+        System.out.println(resultado);
+    }    
+    
+}
