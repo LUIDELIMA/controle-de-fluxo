132f192127f66e9bd5e89084e19f41eec83856de
Luide Lima <luizcarloslimadasilva1@gmail.com>
2023-09-05 17:58:56 -0300

    ExemploForArray

---
+++ src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+       // em array o incici sempre começa com 0 
+        String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (String aluno : alunos){
+            System.out.println("O nome do aluno é: " + aluno);
+        }
+    }
+}
